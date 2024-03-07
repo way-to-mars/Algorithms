9d4e9d1968fe9ffabd44114bd95d6086f736100d
Sergio Wallace <way2mars@hotmail.com>
2024-03-07 08:16:27 +0700

    F solved

---
+++ src/F.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val n = readln().toInt()
+    val input = readln().split(" ").mapNotNull(String::toIntOrNull).take(n)
+
+    var countOdds = if (input[0] % 2 == 0) 0 else 1
+    input.drop(1).map {
+        val isOdd = it % 2 != 0
+        if (isOdd && countOdds < 2) countOdds++
+        if (countOdds < 2) '+' else { if (isOdd) 'x' else '+' }
+    }.joinToString(separator = "").also(::println)
+}
