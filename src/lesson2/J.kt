ae78d9145925a1e7762996c563d18eae57e08e58
way2mars <way2mars@hotmail.com>
2024-03-12 00:12:28 +0700

    J is ready for tests

--- src/lesson2/J.kt
+++ src/lesson2/J.kt
@@ -1,27 +1,113 @@
 package lesson2
 
-fun main(){
-    val (m, n) = readln().split(" ").map(String::toInt)
-    val matrix: Array<CharArray> = Array(m) { readln().toCharArray() }
+fun main() {
+    val picture = Picture()
 
-    println(matrix)
+    if (picture.blackSquares <= 1) {
+        println("NO")
+        return
+    }
 
-    fun firstBlack(): Pair<Int, Int>{
-        for (i in matrix.indices)
-            for(j in matrix[i].indices)
-                if (matrix[i][j] == '#') return i to j
+    val dimOfA = picture.paintRectangle('a')
+    val dimOfB = picture.paintRectangle('b')
+
+    if (picture.blackSquares > 0) {
+        println("NO")
+        return
+    }
+
+    if (dimOfB == 0 to 0) picture.cut(dimOfA, 'a', 'b')
+    println("YES")
+    println(picture)
+}
+
+class Picture {
+    private val m: Int
+    private val n: Int
+    private val matrix: Array<CharArray>
+
+    init {
+        readln().split(" ").map(String::toInt).also {
+            m = it[0]
+            n = it[1]
+        }
+        matrix = Array(m) { readln().toCharArray() }
+    }
+
+    val blackSquares: Int
+        get() = matrix.sumOf { it.count { ch -> ch == '#' } }
+
+    private fun firstChar(char: Char): Pair<Int, Int> {
+        for (i in matrix.indices) for (j in matrix[i].indices) if (matrix[i][j] == char) return i to j
         return -1 to -1
     }
-    fun firstFigure(){
-        val (y, x) = firstBlack()
+
+    fun paintRectangle(char: Char): Pair<Int, Int> {  // returns area of painted area
+        val (x, y) = firstChar('#')
+        if (x < 0 || y < 0) return 0 to 0
+
+        var width = 0
+        for (i in x..<m) {
+            if (matrix[i][y] == '#') {
+                matrix[i][y] = char
+                width++
+            } else break
+        }
+        var height = 1
+        for (j in y + 1..<n) {
+            var hasLine = true
+            for (i in 0..<width) if (matrix[x + i][j] != '#') {
+                hasLine = false
+                break
+            }
+            if (hasLine) {
+                for (i in 0..<width) matrix[x + i][j] = char
+                height++
+            } else break
+        }
+        return width to height
+    }
+
+    fun cut(dimm: Pair<Int, Int>, ch1: Char, ch2: Char) {
+        val (x, y) = firstChar(ch1)
+        val (width, height) = dimm
+
+        if (width == 1) matrix[x][y] = ch2
+        else {
+            for (i in 0..<height) matrix[x][y + i] = ch2
+        }
 
     }
 
+    override fun toString() = matrix.joinToString(separator = "\n") { it.joinToString(separator = "") }
 }
 
-fun println(matrix: Array<CharArray>){
-    matrix.forEach { it.joinToString(separator = "").also(::println) }
-}
+/*
+2 1
+#
+.
+
+2 2
+..
+##
+
+1 3
+###
+
+1 5
+####.
+
+4 3
+.##
+.##
+...
+###
+
+4 3
+##.
+##.
+.##
+...
 
 
-
+ */
