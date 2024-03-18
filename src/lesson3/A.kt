781fed33f13a6832327055e4afdf92cae4e69908
way2mars <way2mars@hotmail.com>
2024-03-18 23:32:29 +0700

    3A is solved. Hint is to use SortedSet

--- src/lesson3/A.kt
+++ src/lesson3/A.kt
@@ -2,14 +2,18 @@ package lesson3
 
 fun main(){
     val n = readln().toInt()  //  1 ≤ n ≤ 2 ⋅ 10^5
-    readln()
-    var tracks = readln().split(" ")
+    readln()  /// ignore it
+    var tracks = readln().split(" ").toSortedSet()
 
     repeat(n - 1){
-        readln()
-        tracks = readln().split(" ").filter { tracks.contains(it) }
+        readln()  /// ignore it
+        val tmp = sortedSetOf<String>()
+        readln().split(" ").forEach {
+            if (tracks.contains(it)) tmp.add(it)
+        }
+        tracks = tmp
     }
 
     println(tracks.size)
-    println(tracks.sorted().joinToString(separator = " "))
+    println(tracks.joinToString(separator = " "))
 }
