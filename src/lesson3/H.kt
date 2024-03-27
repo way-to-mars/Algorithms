d5190478dca36c44e1a59045dc8bc94b6c58ed71
Sergio Wallace <way2mars@hotmail.com>
2024-03-27 19:37:08 +0700

    lesson 3 done

--- src/lesson3/H.kt
+++ src/lesson3/H.kt
@@ -1,37 +1,24 @@
 package lesson3
 
-import kotlin.math.abs
-
 fun main() {
     val n = readln().toInt()    // 1..1000
-    val figureA = (1..n).map {
-        val (x1, y1, x2, y2) = readln().split(" ").map(String::toInt)
-        Stick(x1, y1, x2, y2)
-    }
-    val figureB = (1..n).map {
-        val (x1, y1, x2, y2) = readln().split(" ").map(String::toInt)
-        Stick(x1, y1, x2, y2)
-    }
+    val figureA = (1..n).map { Stick.fromInput(readln()) }
+    val figureB = (1..n).map { Stick.fromInput(readln()) }
 
     val commonVectors = figureA.map(Stick::asVector).toSet() intersect figureB.map(Stick::asVector).toSet()
-
-    if (commonVectors.isEmpty()){
+    if (commonVectors.isEmpty()) {
         println(n)
         return
     }
-
-    val deltaMap = mutableMapOf<DeltaSticks, Int>()
-
-    figureA.forEach { stickA ->
+    val maxMatches = figureA.fold(mutableMapOf<DeltaSticks, Int>()){ map, stickA ->
         figureB.forEach { stickB ->
-            val delta = stickA delta stickB
-            deltaMap[delta] = deltaMap.getOrDefault(delta, 0) + 1
+            val delta = stickA minus stickB
+            map[delta] = map.getOrDefault(delta, 0) + 1
         }
-    }
+        map
+    }.values.max()
 
-    val maxMatches = deltaMap.values.max()
     println(n - maxMatches)
-
 }
 
 data class Point(val x: Int = 0, val y: Int = 0) {
@@ -39,11 +26,28 @@ data class Point(val x: Int = 0, val y: Int = 0) {
 }
 
 class Stick(x1: Int, y1: Int, x2: Int, y2: Int) {
-    val pt1 = Point(x1, y1)
-    val pt2 = Point(x2, y2)
+    private val startPoint: Point
+    private val endPoint: Point
+    init {
+        val pt1 = Point(x1, y1)
+        val pt2 = Point(x2, y2)
 
-    fun asVector() = pt2 minus pt1
-    infix fun delta(other: Stick) = DeltaSticks(other.pt1 minus pt1 , other.pt2 minus pt2)
+        if (x1 < x2 || (x1 == x2 && y1 < y2)) {
+            startPoint = pt1
+            endPoint = pt2
+        } else {
+            startPoint = pt2
+            endPoint = pt1
+        }
+    }
+    companion object {
+        fun fromInput(str: String): Stick {
+            val (x1, y1, x2, y2) = str.split(" ").map(String::toInt)
+            return Stick(x1, y1, x2, y2)
+        }
+    }
+    fun asVector() = endPoint minus startPoint
+    infix fun minus(other: Stick) = DeltaSticks(other.startPoint minus startPoint, other.endPoint minus endPoint)
 }
 
 data class DeltaSticks(val pt1: Point, val pt2: Point)
