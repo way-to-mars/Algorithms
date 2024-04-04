1ecfa32f817702286dab08ffd9cdf4763a311dc9
Sergio Wallace <way2mars@hotmail.com>
2024-04-04 22:02:18 +0700

    CashedFunction : ConcurrentHashMap.getOrPut

--- src/examples/CashedFunction.kt
+++ src/examples/CashedFunction.kt
@@ -1,17 +1,27 @@
 package examples
 
-import kotlin.system.measureTimeMillis
+import java.util.concurrent.ConcurrentHashMap
 
 fun main() {
-    val p2 = { x: Int -> x * x }
+    val p2 = CashedFunction { x: Int ->
+        x * x
+    }.warmUp(1..10)
+
 }
 
 
-class CashedFunction<T, R>(private val func: (T) -> R){
-    private val storage = hashMapOf<T, R>()
+class CashedFunction<T, R>(private val func: (T) -> R) {
+    private val storage = ConcurrentHashMap<T, R>()
+    operator fun invoke(t: T): R = storage.getOrPut(t) { func(t) }
 
-    operator fun invoke(t: T): R {
-        if (!storage.containsKey(t)) storage[t] = func(t)
-        return storage[t]!!
+    fun warmUp(args: Iterable<T>): CashedFunction<T, R> {
+        for (arg in args) storage.getOrPut(arg) { func(arg) }
+        return this
     }
 }
+
+class CashedFunction2<T1, T2, R>(private val func: (T1, T2) -> R) {
+    private val storage = ConcurrentHashMap<Pair<T1, T2>, R>()
+    operator fun invoke(a: T1, b: T2): R = storage.getOrPut(a to b) { func(a, b) }
+}
+
