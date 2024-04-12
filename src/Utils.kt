c2ba73902d35f61d6da444f171ce808f3b88effa
Sergio Wallace <way2mars@hotmail.com>
2024-04-12 19:20:55 +0700

    keep on working on substrings

--- src/Utils.kt
+++ src/Utils.kt
@@ -1,15 +1,20 @@
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-fun Any?.printLog(showTime: Boolean = true, showType: Boolean = true, transform: (Any?) -> String = { it.toString() }): Any?{
-    if (this == null){
+fun <T> T.printLog(
+    showTime: Boolean = true,
+    showType: Boolean = true,
+    transform: (T) -> String = { it.toString() }
+): T {
+    if (this == null) {
         println("null object")
-        return null
+        return this
     }
-
     val type = if (showType) "(${this.javaClass.kotlin.qualifiedName}) = " else ""
-    val time = if (showTime) "[${LocalDateTime.now()
-        .format(DateTimeFormatter.ofPattern("HH:mm:ss.SSSSSS"))}] " else ""
+    val time = if (showTime) "[${
+        LocalDateTime.now()
+            .format(DateTimeFormatter.ofPattern("HH:mm:ss.SSSSSS"))
+    }] " else ""
     println("$time$type${transform(this)}")
     return this
 }
@@ -17,4 +22,13 @@ fun Any?.printLog(showTime: Boolean = true, showType: Boolean = true, transform:
 fun Any?.printLine(): Any? {
     println(this)
     return this
+}
+
+// Быстрое возведение в степень (p >= 0)
+fun Int.fastPow(p: Int): Int {
+    if (p == 0) return 1
+    return if (p % 2 == 0)
+        (this * this).fastPow(p / 2)
+    else
+        this * (this * this).fastPow((p - 1) / 2)
 }
--- src/examples/findSubstring.kt
+++ src/examples/findSubstring.kt
@@ -1,41 +1,101 @@
 package examples
 
-import printLine
+import fastPow
 import printLog
 
-fun main(){
-    val str1 = "Little cat, little cat, I have no flat"
-    val str2 = "cat"
-    findSubstringsBruteForce(str1.toList(), str2.toList()).printLog { (it as List<Int>).joinToString(separator = ": ") }
+fun main() {
+    val str1 = "Little mouse, little mouse, Where is your house? Little cat, little cat, I have no flat. " +
+            "I am a poor mouse, I have no house. Little mouse, little mouse," +
+            "Come into my house. " +
+            "Little cat, little cat, " +
+            "I cannot do that, " +
+            "You want to eat me!"
+    val str2 = "ou"
 
-    findSubstringsRabinCarp(str1.toList(), str2.toList())
+    findSubstringsBruteForce(str1.toList(), str2.toList()).printLog()
+    findSubstringsRabinCarp(str1.toList(), str2.toList()).printLog()
+
+    str1.indexOf(str2).printLog()
+
+    findSubstringsKMP("лилилилил".toList(), "ииииииии".toList()).printLog()
 }
 
+/**
+ * Прямой поиск подстроки в строке. Сложность O(n*m)
+ */
 fun <T> findSubstringsBruteForce(where: List<T>, what: List<T>): List<Int> =
     (0..<where.size - what.size).filter { i ->
-        !what.indices.any { what[it] != where[it + i]}
+        !what.indices.any { what[it] != where[it + i] }
     }
 
-fun <T> findSubstringsRabinCarp(where: List<T>, what: List<T>): List<Int>{
-    val q: Int = 2147483647
 
-    val subHash = what.fold(0) { sum, i -> sum * 31 + i.hashCode() }.mod(q).printLog()
-    var curHash = Sequence{ where.iterator() }.take(what.size).fold(0) { sum, i -> sum * 31 + i.hashCode() }.mod(q).printLog()
+/**
+ * Алгоритм Рабина — Карпа — это алгоритм поиска строки, который ищет шаблон, то есть подстроку, в тексте,
+ * используя хеширование.
+ * Для текста длины n и шаблона длины m его среднее и лучшее время исполнения равно O(n) при правильном выборе
+ * хеш-функции, но в худшем случае он имеет эффективность O(nm), что является одной из причин того, почему он
+ * не слишком широко используется.
+ * Из-за медленного поведения в худшем случае алгоритм Рабина — Карпа хуже алгоритма Кнута — Морриса — Пратта,
+ * алгоритма Бойера — Мура и других быстрых алгоритмов поиска строк. Тем не менее, алгоритм Рабина — Карпа можно
+ * использовать для поиска набора образцов за линейное время в лучшем случае и квадратичное в труднодостижимом
+ * худшем случае; хотя и здесь он проигрывает в худшем случае алгоритму Ахо — Корасик, имеющему линейное время работы.
+ */
+fun <T> findSubstringsRabinCarp(source: List<T>, sub: List<T>): List<Int> {
+    if (sub.isEmpty() || source.isEmpty() || source.size < sub.size) return emptyList()
 
-    println("-------")
+    val q: Int = 2147483647  // 64-bit = 2^61-1 = 2305843009213693951
+    val p: Int = 31 // 2^5 - 1
+    val pm = p.fastPow(sub.size - 1)
 
+    val subIndices = sub.indices
+    val subHash = sub.fold(0) { sum, it -> sum * p + it.hashCode() }.mod(q)
+    var curHash = subIndices.fold(0) { sum, i -> sum * p + source[i].hashCode() }.mod(q)
+    val res = mutableListOf<Int>()
     var index = 0
-    val count = where.size - what.size
-    while (true){
+    var rightIndex = sub.size - 1
+    while (true) {
+        if (curHash == subHash) {
+            if (!subIndices.any { i -> source[i + index] != sub[i] }) res.add(index)    // compare substrings
+//            else println("Hash collision")
+        }
+        rightIndex++
+        if (rightIndex >= source.size) break
+        curHash = ((curHash - pm * source[index].hashCode()) * p + source[rightIndex].hashCode()).mod(q)
+        index++
+//        // check:
+//        val curHashStrict = subIndices.fold(0) { sum, i -> sum * p + source[i + index].hashCode() }.mod(q)
+//        if(curHashStrict != curHash) "different hashes $curHash != $curHashStrict".printLog()
+    }
+    return res
+}
 
-        index ++
-        if (index >= count) break
-        // calc new hash
-        curHash = Sequence{ where.iterator() }
-            .drop(index).take(what.size)
-            .fold(0) { sum, i -> sum * 31 + i.hashCode() }.mod(q).printLog()
+fun <T> findSubstringsKMP(source: List<T>, sub: List<T>): List<Int> {
+    if (sub.isEmpty() || source.isEmpty() || source.size < sub.size) return emptyList()
+    if (sub.size == 1) return source.indices.filter { source[it] == sub.first() }
+
+    val pi = IntArray(sub.size).also {  // sub.size > 1
+        it[0] = 0
+        var j = 0
+        var i = 1
+        while (i < sub.size){
+            if (sub[i] != sub[j]){
+                if (j == 0){
+                    it[i] = 0
+                    i++
+                }
+                else j = it[j - 1]
+            }
+            else{
+                if(sub[i] == sub[j]){
+                    it[i] = j + 1
+                    i++
+                    j++
+                }
+            }
+        }
     }
 
+    pi.printLog { it.contentToString() }
 
     return emptyList()
 }
